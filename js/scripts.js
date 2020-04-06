ae637f291786cd0eb5538cccedc065110657a957
Spencer Moody <spencer.moody@outlook.com>
2020-04-06 15:19:09 -0700

    Add buisness logic for List and ToDo/UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 // Business Logic for List -------
-function List(){
-  this.toDo = []
-  this.currentId = 0
+var list = {
+  toDos: [],
+  currentId: 0,
 }
 
+ List.prototype.addToDo = function(toDo) {
+   toDo.id = this.assignId();
+   this.toDos.Push(toDo)
+ }
+ List.prototype.assignId = function(){
+   this.currentId += 1;
+   return this.currentId;
+ }
+ List.prototype.deleteToDo = function(id){
+   for (var i=0; i<this.toDos.length; i++){
+     if (this.toDos[i]){
+       if (this.toDos[i.id === id]) {
+         delete this.toDos[i];
+         return true; 
+       }
+     }
+   }
+   return false;
+ }
 
 
 // Business logic for toDo -------
-function toDo(project, status){
+function ToDo(project, status){
   this.project = project,
   this.status = status
 }
 
 // User Interface Logic -------
+$(document).ready(function(){
+  $("form#to-do-list").submit(function(event){
+    event.preventDefault();
+    
+    var projectInput = $("input#project").val();
+    var statusInput = "incomplete";
+    var project = new ToDo(projectInput, statusInput);
+    debugger;
+    list.addToDo(project);
+    console.log(List.toDos);
+  });
+});
 

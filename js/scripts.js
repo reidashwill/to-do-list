359d38060f98b1554cb281ee654b5dc80a8377d0
Spencer Moody <spencer.moody@outlook.com>
2020-04-06 14:53:41 -0700

    Add forms to index.html and some Buisness Logic/Constructs
    co-authored-by: Reid-Ashwill <reidashwill@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic for List -------
+function List(){
+  this.toDo = []
+  this.currentId = 0
+}
+
+
+
+// Business logic for toDo -------
+function toDo(project, status){
+  this.project = project,
+  this.status = status
+}
+
+// User Interface Logic -------
+
